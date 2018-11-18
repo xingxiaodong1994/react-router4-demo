52e86590d7baa8859ff7e391e1c17d6fe4172831
xingxiaodong1994 <1204642219@qq.com>
2018-11-18 09:31:40 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react'
-import Child from './Child'
-import {Provider} from 'react-redux'
-import store from './data/store'
-
 class App extends Component{
     render(){
         return (
-            <Provider store={store}>
+        
                 <div>
                     这里是App组件
-                    <Child />
                 </div>
-            </Provider>
+          
         )
     }
 }
