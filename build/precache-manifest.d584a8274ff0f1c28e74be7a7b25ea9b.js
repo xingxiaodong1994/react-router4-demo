9a10cb37ca9228a10d65baf80febc6a60439cf97
xingxiaodong1994 <1204642219@qq.com>
2018-11-21 16:18:06 +0800

    build

---
+++ build/precache-manifest.d584a8274ff0f1c28e74be7a7b25ea9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d762570ad2fa9995bad",
+    "url": "/react-router4-demo/build/static/js/runtime~main.5d762570.js"
+  },
+  {
+    "revision": "ee7e79ef1fbb5c18ebcf",
+    "url": "/react-router4-demo/build/static/js/main.ee7e79ef.chunk.js"
+  },
+  {
+    "revision": "d91da9ed6e1994cc7aa1",
+    "url": "/react-router4-demo/build/static/js/1.d91da9ed.chunk.js"
+  },
+  {
+    "revision": "ee7e79ef1fbb5c18ebcf",
+    "url": "/react-router4-demo/build/static/css/main.1ed560e7.chunk.css"
+  },
+  {
+    "revision": "38917ea74bea164801aa2b04b3534f12",
+    "url": "/react-router4-demo/build/index.html"
+  }
+];
